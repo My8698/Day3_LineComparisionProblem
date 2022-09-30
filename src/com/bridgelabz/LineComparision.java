82e8875b066240629ce4a50fed099630e3a084d0
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2022-10-01 00:12:17 +0530

    Added:Calculated length of line

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -1,10 +1,26 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.lang.Math;
 public class LineComparision {
-    public static void main(String[] args) {
 
+        public static int LengthOfLine() {
 
-        System.out.println( "Welcome to line Comparision Problem");
+            int x1,y1,x2,y2;
+            Scanner scn = new Scanner(System.in);
+            System.out.println("Enter the line co-ordinates: x1,y1,x2,y2");
+            x1 = scn.nextInt();
+            y1 = scn.nextInt();
+            x2 = scn.nextInt();
+            y2 = scn.nextInt();
+            int lineLength =(int)Math.sqrt(Math.pow((x2-x1), 2) + Math.pow((y2-y1), 2) );
+            return lineLength;
+
+        }
+        public static void main(String[] args) {
+            int lineLength = LengthOfLine();
+            System.out.println("The length of line is: "+lineLength);
+        }
 
     }
-}
+
